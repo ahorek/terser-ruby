701f89669e83d2a0ee8ad136db7a225f47ef2b0c
pavel <pavel.rosicky@easy.cz>
2020-08-02 15:01:56 +0200

    benchmark

--- benchmark/compare.rb
+++ benchmark/compare.rb
@@ -13,8 +13,21 @@ end
 require 'terser'
 require 'uglifier'
 
-content = File.binread(ARGV[0])
-puts 'size:'
+terser = Terser.new
+uglifier = Uglifier.new
+harmony_uglifier = Uglifier.new(harmony: true)
+content = File.binread(ARGV[0]) rescue nil
+content ||= <<-JS
+function Foo(a, b) {
+  this.a = a;
+  this.b = b;
+}
+Foo.prototype.show = function () {
+  alert(this.a);
+};
+JS
+puts ''
+puts "original size: #{content.size}"
 puts "terser: #{Terser.compile(content).size}"
 puts "uglifier: #{Uglifier.compile(content).size}"
 puts "uglifier harmony: #{Uglifier.compile(content, harmony: true).size}"
@@ -25,5 +38,8 @@ Benchmark.ips do |x|
   x.report("terser") { Terser.compile(content) }
   x.report("uglifier") { Uglifier.compile(content) }
   x.report("uglifier harmony") { Uglifier.compile(content, harmony: true) }
+  x.report("terser precompiled") { terser.compile(content) }
+  x.report("uglifier precompiled") { uglifier.compile(content) }
+  x.report("uglifier harmony precompiled") { harmony_uglifier.compile(content) }
   x.compare!
 end
